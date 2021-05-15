e9cf0e1529403ff38acd4f493c2306b34168a51b
Tim Zimmermann <tim@linux4.de>
2021-05-15 11:32:03 +0200

    wifi: optionally reset the interface after setMacAddress
    * Some wifi chips need this to properly work with mac randomization
      like samsung devices using bcmdhd
    * Without this they're just stuck at "Authenticating"
      and eventually disconnect
    
    Change-Id: I11ea0218c96bb8621597548c52463b7f2fd593d6

--- wifi/1.4/default/Android.mk
+++ wifi/1.4/default/Android.mk
@@ -42,6 +42,9 @@ endif
 ifdef QC_WIFI_HIDL_FEATURE_DUAL_AP
 LOCAL_CPPFLAGS += -DQC_WIFI_HIDL_FEATURE_DUAL_AP
 endif
+ifdef WIFI_RESET_IFACE_AFTER_MAC_CHANGE
+LOCAL_CPPFLAGS += -DWIFI_RESET_IFACE_AFTER_MAC_CHANGE
+endif
 # Allow implicit fallthroughs in wifi_legacy_hal.cpp until they are fixed.
 LOCAL_CFLAGS += -Wno-error=implicit-fallthrough
 LOCAL_SRC_FILES := \
